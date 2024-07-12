78e4fd9d42b90bc3110b7419630dd011471e20b7
Gabriel Herrera <gabriel.herrera@jbknowledge.com>
2024-07-12 09:46:47 -0300

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
+
+    const onAddCategory = ( newCategory ) => {
+
+        if( categories.includes( newCategory ) ) return;
+        setCategories([ newCategory, ...categories ]);
+    }
+
+  return (
+    <>
+        <h1>Gif Expert App</h1>
+
+        <AddCategory 
+            // setcategories={ setcategories }
+            onNewCategory={ (value) => onAddCategory( value ) } 
+        />
+        
+        { 
+            categories.map( (category) => (
+                
+                <GifGrid 
+                    key={ category } 
+                    category={ category } 
+                ><h1>category</h1></GifGrid>
+            ))
+        }
+            
+      
+    </>
+  )
+}
